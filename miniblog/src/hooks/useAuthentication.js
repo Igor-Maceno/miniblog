fc39c902de34da92f580e2e29c157e04ee9f911c
Igor-Maceno <igorlopesmaceno17@gmail.com>
2023-06-14 15:53:26 -0300

    User Register
    Registro de usuário no sistema.

--- miniblog/src/hooks/useAuthentication.js
+++ miniblog/src/hooks/useAuthentication.js
@@ -1,25 +1,77 @@
-import{
-    getAuth,
-    createUserWithEmailAndPassword,
-    updateProfile,
-    signOut
-} from 'firebase/auth';
+import { db } from '../firebase/config'
 
-import { useState, useEffect } from 'react';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  signOut,
+} from "firebase/auth";
 
-export const useAuthentication = () =>{
-    const [error, setError] = useState(null);
-    const [loading, serLoading] = useState(null);
+import { useState, useEffect } from "react";
 
-    //cleanuo
-    //deal with memory leak
-    const [cancelled, setCancelled] = useState(false)
+export const useAuthentication = () => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
 
-    const auth = getAuth()
+  //cleanuo
+  //deal with memory leak
+  const [cancelled, setCancelled] = useState(false);
 
-    function checkIfIsCanceled(){
-        if(cancelled){
-            return;
-        }
+  const auth = getAuth();
+
+  function checkIfIsCanceled() {
+    if (cancelled) {
+      return;
     }
-}
+  }
+
+  const createUser = async (data) => {
+    checkIfIsCanceled();
+
+    setLoading(true);
+    setError(null);
+
+    try {
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+
+      await updateProfile(user, {
+        displayName: data.displayName,
+      });
+
+      setLoading(false);
+
+      return user;
+    } catch (error) {
+      console.log(error.message);
+      console.log(typeof error.message);
+
+      let systemMessageError
+
+      if(error.message.includes("password")){
+        systemMessageError = "A senha deve conter pelo menos 6 caracteres."  
+      } else if(error.message.includes("email-already")){
+        systemMessageError = "E-mail já cadastrado."
+      } else {
+        systemMessageError = "Ocorreu um erro, tente novamente mais tarde!"
+      }
+
+      setLoading(false)
+      setError(systemMessageError)
+    }
+  };
+
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
+
+  return {
+    auth,
+    createUser,
+    error,
+    loading,
+  };
+};
--- miniblog/src/pages/Register/Register.js
+++ miniblog/src/pages/Register/Register.js
@@ -1,3 +1,4 @@
+import { useAuthentication } from "../../hooks/useAuthentication";
 import styles from "./Register.module.css";
 import { useState, useEffect } from "react";
 
@@ -8,24 +9,34 @@ function Register() {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
+  const { createUser, error: authError, loading } = useAuthentication();
 
-    setError("")
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setError("");
 
     const user = {
       displayName,
       email,
-      password
-    }
+      password,
+    };
 
-    if(password !== confirmPassword){
+    if (password !== confirmPassword) {
       setError("As senhas não conferem!");
-      return
+      return;
     }
 
-    console.log(user);
-  }
+    const res = await createUser(user);
+
+    console.log(res);
+  };
+
+  useEffect(() => {
+    if (authError) {
+      setError(authError);
+    }
+  }, [authError]);
 
   return (
     <div className={styles.register}>
@@ -40,7 +51,7 @@ function Register() {
             required
             placeholder="Nome do usuário"
             value={displayName}
-            onChange={(e)=>setDisplayName(e.target.value)}
+            onChange={(e) => setDisplayName(e.target.value)}
           />
         </label>
         <label>
@@ -51,7 +62,7 @@ function Register() {
             required
             placeholder="E-mail do usuário"
             value={email}
-            onChange={(e)=> setEmail(e.target.value)}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </label>
         <label>
@@ -62,7 +73,7 @@ function Register() {
             required
             placeholder="Insira sua senha"
             value={password}
-            onChange={(e)=>setPassword(e.target.value)}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </label>
         <label>
@@ -73,10 +84,15 @@ function Register() {
             required
             placeholder="Confirme a sua senha"
             value={confirmPassword}
-            onChange={(e)=> setConfirmPassword(e.target.value)}
+            onChange={(e) => setConfirmPassword(e.target.value)}
           />
         </label>
-        <button className="btn">Cadastrar</button>
+        {!loading && <button className="btn">Cadastrar</button>}
+        {loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
         {error && <p className="error">{error}</p>}
       </form>
     </div>
