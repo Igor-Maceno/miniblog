1712a3e38899dbe8dd62307f33b29c177975237e
Igor-Maceno <igorlopesmaceno17@gmail.com>
2023-06-20 16:18:41 -0300

    Dashboad
    Estruturação da dashboard.

--- miniblog/src/pages/Dashboard/Dashboard.js
+++ miniblog/src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,32 @@
 import styles from './Dashboard.module.css'
 
+import { Link } from 'react-router-dom';
+
+//hooks
+import { useAuthValue } from '../../context/AuthContext';
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
+
 const Dashboard = () => {
+  const { user } = useAuthValue();
+  const uid = user.id;
+
+  //user posts
+  const posts = [];
+
   return (
     <div>
         <h2>Dashboard</h2>
+        <p>Gerencie seus posts</p>
+        {posts && posts.length === 0 ? (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts!</p>
+            <Link to="/post/create" className='btn'>Criar primeiro post</Link>
+          </div>
+        ) : (
+          <div>
+            <p>Tem posts!</p>
+          </div>
+        )}
     </div>
   )
 }
